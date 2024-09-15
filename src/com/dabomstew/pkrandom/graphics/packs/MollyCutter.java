d5559e7b12ae36a834f20a49ee4c82df438b4e4c
voliol <30631214+voliol@users.noreply.github.com>
2024-09-15 21:45:21 +0200

    implement class for cutting up spritesheets by MollyChan

---
+++ src/com/dabomstew/pkrandom/graphics/packs/MollyCutter.java
@@ -0,0 +1,83 @@
+package com.dabomstew.pkrandom.graphics.packs;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Cuts up the images by MollyChan on spriter's resource
+ */
+public class MollyCutter {
+
+    private static final int EXTRA_MARGIN = 4;
+    private static final int SECTION_WIDTH = 112;
+
+    private static final int FRONT_DIM = 56;
+    private static final int BACK_DIM = 48;
+    private static final int OV_DIM = 16;
+    private static final int FISH_WIDTH = 16;
+    private static final int FISH_HEIGHT = 8;
+
+    boolean monoSection;
+
+    public static void main(String[] args) throws IOException {
+        new MollyCutter().cut();
+    }
+
+    private void cut() throws IOException {
+        BufferedImage in = ImageIO.read(new File("in.png"));
+        in = in.getSubimage(EXTRA_MARGIN, 0, in.getWidth() - EXTRA_MARGIN , in.getHeight());
+
+        monoSection = in.getWidth() <= SECTION_WIDTH;
+        int i = 1;
+        while (in.getWidth() >= SECTION_WIDTH) {
+            System.out.println("Section " + i);
+            BufferedImage front = in.getSubimage(5,3, FRONT_DIM, FRONT_DIM);
+            ImageIO.write(front, "png", fromName("front", i));
+
+            BufferedImage back = in.getSubimage(64,11, BACK_DIM, BACK_DIM);
+            ImageIO.write(back, "png", fromName("back", i));
+
+            BufferedImage walk = new BufferedImage(OV_DIM, OV_DIM * 6, 1);
+            Graphics walkG = walk.getGraphics();
+            walkG.drawImage(in.getSubimage(8, 62, OV_DIM, OV_DIM), 0, 0, null);
+            walkG.drawImage(in.getSubimage(25, 62, OV_DIM, OV_DIM), 0, OV_DIM, null);
+            walkG.drawImage(in.getSubimage(42, 62, OV_DIM, OV_DIM), 0, OV_DIM * 2, null);
+            walkG.drawImage(in.getSubimage(8, 79, OV_DIM, OV_DIM), 0, OV_DIM * 3, null);
+            walkG.drawImage(in.getSubimage(25, 79, OV_DIM, OV_DIM), 0, OV_DIM * 4, null);
+            walkG.drawImage(in.getSubimage(42, 79, OV_DIM, OV_DIM), 0, OV_DIM * 5, null);
+            ImageIO.write(walk, "png", fromName("walk", i));
+
+            BufferedImage bike = new BufferedImage(OV_DIM, OV_DIM * 6, 1);
+            Graphics bikeG = bike.getGraphics();
+            bikeG.drawImage(in.getSubimage(59, 62, OV_DIM, OV_DIM), 0, 0, null);
+            bikeG.drawImage(in.getSubimage(76, 62, OV_DIM, OV_DIM), 0, OV_DIM, null);
+            bikeG.drawImage(in.getSubimage(93, 62, OV_DIM, OV_DIM), 0, OV_DIM * 2, null);
+            bikeG.drawImage(in.getSubimage(59, 79, OV_DIM, OV_DIM), 0, OV_DIM * 3, null);
+            bikeG.drawImage(in.getSubimage(76, 79, OV_DIM, OV_DIM), 0, OV_DIM * 4, null);
+            bikeG.drawImage(in.getSubimage(93, 79, OV_DIM, OV_DIM), 0, OV_DIM * 5, null);
+            ImageIO.write(bike, "png", fromName("bike", i));
+
+            BufferedImage fish = new BufferedImage(FISH_WIDTH, FISH_HEIGHT * 3, 1);
+            Graphics fishG = fish.getGraphics();
+            fishG.drawImage(in.getSubimage(8, 104, FISH_WIDTH, FISH_HEIGHT), 0, 0, null);
+            fishG.drawImage(in.getSubimage(25, 112, FISH_WIDTH, FISH_HEIGHT), 0, FISH_HEIGHT, null);
+            fishG.drawImage(in.getSubimage(50, 112, FISH_WIDTH, FISH_HEIGHT), 0, FISH_HEIGHT * 2, null);
+            ImageIO.write(fish, "png", fromName("fish", i));
+
+            in = in.getSubimage(SECTION_WIDTH, 0, in.getWidth() - SECTION_WIDTH, in.getHeight());
+            i++;
+        }
+
+        System.out.println("Done!");
+    }
+
+    private File fromName(String name, int i) {
+        File f =  new File(name + (monoSection ? "" : "_" + i) + ".png");
+        System.out.println(f);
+        return f;
+    }
+
+}

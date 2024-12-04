a04c1c9c0cb10b1c41fb5aa8b2221f6b050f47cc
Stella Hack <foxoftheasterisk@gmail.com>
2024-12-03 21:22:24 -0800

    complete EnumSettingDefinition

--- src/com/dabomstew/pkrandom/settings/EnumSettingDefinition.java
+++ src/com/dabomstew/pkrandom/settings/EnumSettingDefinition.java
@@ -8,14 +8,35 @@ import java.util.function.Predicate;
 public class EnumSettingDefinition<T extends Enum<T>> extends SettingDefinition<T> {
 
     Map<T, SettingRestriction> restrictions;
+    Map<T, Predicate<RomHandler>> support;
 
     public EnumSettingDefinition(String name, String category, T defaultValue, SettingRestriction prerequisite,
-                                 Predicate<RomHandler> supported, Map<T, SettingRestriction> restrictions) {
+                                 Predicate<RomHandler> supported, Map<T, SettingRestriction> restrictedStates,
+                                 Map<T, Predicate<RomHandler>> supportedStates) {
         super(name, category, defaultValue, prerequisite, supported);
-        this.restrictions = restrictions;
+        this.restrictions = restrictedStates;
+        support = supportedStates;
     }
 
     public boolean isValueEnabled(T value) {
-        return restrictions.get(value).test(manager);
+        if(restrictions == null) {
+            return true;
+        }
+        SettingRestriction restriction = restrictions.get(value);
+        if(restriction == null) {
+            return true;
+        }
+        return restriction.test(manager);
+    }
+
+    public boolean isValueSupported(T value, RomHandler game) {
+        if(support == null) {
+            return true;
+        }
+        Predicate<RomHandler> supportTest = support.get(value);
+        if(supportTest == null) {
+            return true;
+        }
+        return supportTest.test(game);
     }
 }

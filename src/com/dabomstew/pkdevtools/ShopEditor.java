9de36cbb801bbbb14cc3ade55a8d296f51c83520
voliol <30631214+voliol@users.noreply.github.com>
2025-06-20 12:25:17 +0200

    adding items and warning pre-save

--- src/com/dabomstew/pkdevtools/ShopEditor.java
+++ src/com/dabomstew/pkdevtools/ShopEditor.java
@@ -22,9 +22,12 @@ public class ShopEditor {
 
     private final RomOpener romOpener = new RomOpener();
 
+    private File romFile;
     private RomHandler romHandler;
     private List<Shop> shops;
     private List<Integer> prices;
+
+    private boolean hasAddedItems;
 
     private final JFrame frame;
     private final JComboBox<String> shopComboBox;
@@ -103,26 +106,33 @@ public class ShopEditor {
             return;
         }
 
-        File romFile = romChooser.getSelectedFile();
+        File tryFile = romChooser.getSelectedFile();
+        loadROM(tryFile, false);
+    }
+
+    private void loadROM(File tryFile, boolean reload) {
         try {
-            RomOpener.Results results = romOpener.openRomFile(romFile);
+            RomOpener.Results results = romOpener.openRomFile(tryFile);
             if (results.wasOpeningSuccessful()) {
                 if (results.getRomHandler().hasShopSupport()) {
+                    romFile = tryFile;
                     romHandler = results.getRomHandler();
-                    romOpened();
+                    if (!reload) {
+                        romOpened();
+                    }
                 } else {
                     JOptionPane.showMessageDialog(frame,
-                            romFile.getName() + " could be opened, but does not have shop editing support.",
-                            "Invalid ROM", JOptionPane.WARNING_MESSAGE);
+                            tryFile.getName() + " could be opened, but does not have shop editing support.",
+                            "Invalid ROM", JOptionPane.ERROR_MESSAGE);
                 }
             } else {
                 JOptionPane.showMessageDialog(frame,
-                        romFile.getName() + " could not be opened. Reason: " + results.getFailType(),
+                        tryFile.getName() + " could not be opened.\nReason: " + results.getFailType(),
                         "Could not open ROM", JOptionPane.ERROR_MESSAGE);
             }
         } catch (Exception e) {
             JOptionPane.showMessageDialog(frame,
-                    romFile.getName() + " could not be opened. An exception was thrown: " + e,
+                    tryFile.getName() + " could not be opened.\nAn exception was thrown: " + e,
                     "Could not open ROM", JOptionPane.ERROR_MESSAGE);
         }
     }
@@ -146,6 +156,19 @@ public class ShopEditor {
     }
 
     private void saveROM() {
+        try {
+            romHandler.setShops(shops);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(frame,
+                    "The shops could not be written to ROM.\n " +
+                            "This is probably because too many items were added,\n" +
+                            "causing the ROM to run out of space. Try removing some\n" +
+                            "items, before attempting to save again.\n" +
+                            "If you did not add any items, please file a bug report.\n\n" +
+                            "The ROM will now be reloaded (this may take a few seconds).",
+                    "Could not write shops.", JOptionPane.ERROR_MESSAGE);
+            loadROM(romFile, true);
+        }
         // TODO
         System.out.println("saving ROM (not yet implemented)");
     }
@@ -173,7 +196,26 @@ public class ShopEditor {
     }
 
     private void addItem() {
-        // TODO
-        System.out.println("adding item (not yet implemented)");
+        if (!hasAddedItems) {
+            int answer = JOptionPane.showConfirmDialog(frame,
+                            "All ROMs have limited space for storing shop items.\n" +
+                                    "This program was made to support adding one (1) item\n" +
+                                    "to every shop in the game; adding any more is done at own risk.\n" +
+                                    "If too many items are added, the ROM will not be able to be saved.\n\n" +
+                                    "Will you add an item? (this dialog will only be shown once)",
+                                "Will you add an item?", JOptionPane.YES_NO_OPTION);
+            if (answer == JOptionPane.NO_OPTION) {
+                return;
+            }
+        }
+        Item defaultItem = romHandler.getItems().get(1);
+
+        Shop currShop = shops.get(shopComboBox.getSelectedIndex());
+        currShop.getItems().add(defaultItem);
+
+        DefaultTableModel tableModel = (DefaultTableModel) shopTable.getModel();
+        tableModel.insertRow(tableModel.getRowCount(), new Object[] {defaultItem, prices.get(defaultItem.getId())});
+
+        hasAddedItems = true;
     }
 }

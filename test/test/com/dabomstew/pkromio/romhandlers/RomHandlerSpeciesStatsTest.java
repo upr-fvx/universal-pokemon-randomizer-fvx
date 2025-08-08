aa48d93b50b0ff55365113ae47bcd5b8cd6be023
voliol <30631214+voliol@users.noreply.github.com>
2025-08-08 14:53:56 +0200

    write heldItemsDoNotChangeWithLoadAndSave() test

--- test/test/com/dabomstew/pkromio/romhandlers/RomHandlerSpeciesStatsTest.java
+++ test/test/com/dabomstew/pkromio/romhandlers/RomHandlerSpeciesStatsTest.java
@@ -1,5 +1,6 @@
 package test.com.dabomstew.pkromio.romhandlers;
 
+import com.dabomstew.pkromio.gamedata.Item;
 import com.dabomstew.pkromio.gamedata.Species;
 import com.dabomstew.pkromio.romhandlers.AbstractRomHandler;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -12,7 +13,6 @@ import java.util.Objects;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RomHandlerSpeciesStatsTest extends RomHandlerTest {
-
 
     private static class BaseStatRecord {
         private final int hp, attack, defense, spatk, spdef, speed, special;
@@ -46,6 +46,35 @@ public class RomHandlerSpeciesStatsTest extends RomHandlerTest {
         }
     }
 
+    private static class HeldItemsRecord {
+        private final Item guaranteed, common, rare, darkGrass;
+
+        public HeldItemsRecord(Species pk) {
+            this.guaranteed = pk.getGuaranteedHeldItem();
+            this.common = pk.getCommonHeldItem();
+            this.rare = pk.getRareHeldItem();
+            this.darkGrass = pk.getDarkGrassHeldItem();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (!(o instanceof HeldItemsRecord)) return false;
+            HeldItemsRecord that = (HeldItemsRecord) o;
+            return Objects.equals(guaranteed, that.guaranteed) && Objects.equals(common, that.common)
+                    && Objects.equals(rare, that.rare) && Objects.equals(darkGrass, that.darkGrass);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(guaranteed, common, rare, darkGrass);
+        }
+
+        @Override
+        public String toString() {
+            return String.format("{%s/%s/%s/%s}", guaranteed, common, rare, darkGrass);
+        }
+    }
+
     @ParameterizedTest
     @MethodSource("getRomNames")
     public void baseStatsDoNotChangeWithLoadAndSave(String romName) {
@@ -62,6 +91,25 @@ public class RomHandlerSpeciesStatsTest extends RomHandlerTest {
         for (Species pk : romHandler.getSpeciesSetInclFormes()) {
             System.out.println(pk.getFullName());
             assertEquals(records.get(pk), new BaseStatRecord(pk));
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("getRomNames")
+    public void heldItemsDoNotChangeWithLoadAndSave(String romName) {
+        loadROM(romName);
+        // it always loads the base stats once
+
+        Map<Species, HeldItemsRecord> records = new HashMap<>();
+        romHandler.getSpeciesSetInclFormes()
+                .forEach(pk -> records.put(pk, new HeldItemsRecord(pk)));
+
+        ((AbstractRomHandler) romHandler).savePokemonStats();
+        ((AbstractRomHandler) romHandler).loadPokemonStats();
+
+        for (Species pk : romHandler.getSpeciesSetInclFormes()) {
+            System.out.println(pk.getFullName());
+            assertEquals(records.get(pk), new HeldItemsRecord(pk));
         }
     }
 

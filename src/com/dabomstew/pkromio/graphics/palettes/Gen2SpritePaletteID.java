68c8df4fd913d70e51a1c4e2ee656199f2f1e38b
voliol <30631214+voliol@users.noreply.github.com>
2025-08-30 18:52:06 +0200

    hardcode the Gen2 sprite palettes, create a method for recognizing them

--- src/com/dabomstew/pkromio/graphics/palettes/Gen2SpritePaletteID.java
+++ src/com/dabomstew/pkromio/graphics/palettes/Gen2SpritePaletteID.java
@@ -7,5 +7,47 @@ package com.dabomstew.pkromio.graphics.palettes;
  * "PINK" is unused in the vanilla games.
  */
 public enum Gen2SpritePaletteID {
-    RED, BLUE, GREEN, BROWN, PINK, EMOTE, TREE, ROCK
+    RED  (new Palette(new int[]{0x00000000, 0xFFF89850, 0xFFF83808, 0xFF000000})),
+    BLUE (new Palette(new int[]{0x00000000, 0xFFF89850, 0xFF5048F8, 0xFF000000})),
+    GREEN(new Palette(new int[]{0x00000000, 0xFFF89850, 0xFF35B721, 0xFF000000})),
+    BROWN(new Palette(new int[]{0x00000000, 0xFFF89850, 0xFF785018, 0xFF000000})),
+    PINK (new Palette(new int[]{0x00000000, 0xFFF89850, 0xFFF05030, 0xFF000000})),
+    EMOTE(new Palette(new int[]{0x00000000, 0xFFF8F8F8, 0xFF686868, 0xFF000000})),
+    TREE (new Palette(new int[]{0x00000000, 0xFF60C808, 0xFF287000, 0xFF383838})),
+    ROCK (new Palette(new int[]{0x00000000, 0xFFC09038, 0xFFA87830, 0xFF383838}));
+
+    /**
+     * Returns the {@link Gen2SpritePaletteID} that matches "close enough",
+     * to the given {@link Palette}. If none match close enough, returns null.
+     */
+    public static Gen2SpritePaletteID getMatching(Palette palette) {
+        if (palette.size() != 4) {
+            return null;
+        }
+        for (Gen2SpritePaletteID pid : values()) {
+            boolean matching = true;
+            for (int col = 1; col < 4; col++) {
+                for (int comp = 0; comp < 3; comp++) {
+                    if (palette.get(col).getComp(comp) - pid.palette.get(col).getComp(comp) >= 8) {
+                        matching = false;
+                    }
+                }
+            }
+            if (matching) {
+                return pid;
+            }
+        }
+        return null;
+    }
+
+    private final Palette palette;
+
+    Gen2SpritePaletteID(Palette palette) {
+        this.palette = palette;
+    }
+
+    public Palette getPalette() {
+        return palette;
+    }
+
 }

3652449bcd3b0148189e587bd462d14019f1277f
voliol <30631214+voliol@users.noreply.github.com>
2026-01-23 16:19:49 +0100

    (SheetTool) auto-detecting frames

--- src/com/dabomstew/pkdevtools/SheetTool.java
+++ src/com/dabomstew/pkdevtools/SheetTool.java
@@ -6,15 +6,17 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SheetTool {
 
-    private static class SheetImageLabel extends JLabel {
+    private static class ImageLabel extends JLabel {
 
-        private BufferedImage bufferedImage;
+        protected BufferedImage bim;
         private final ImageIcon icon;
 
-        private SheetImageLabel(BufferedImage bufferedImage) {
+        private ImageLabel(BufferedImage bufferedImage) {
             if (bufferedImage == null) {
                 bufferedImage = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
             }
@@ -23,17 +25,104 @@ public class SheetTool {
             setImage(bufferedImage);
         }
 
-        public void setImage(BufferedImage bufferedImage) {
-            this.bufferedImage = bufferedImage;
+        public void setImage(BufferedImage bim) {
+            this.bim = bim;
             update();
         }
 
         private void update() {
-            icon.setImage(bufferedImage);
+            icon.setImage(bim);
             revalidate();
             repaint();
         }
+    }
 
+    private static class SheetImageLabel extends ImageLabel {
+
+        private static class Frame {
+            int x, y, w, h;
+            private Frame(int x, int y, int w, int h) {
+                this.x = x;
+                this.y = y;
+                this.w = w;
+                this.h = h;
+            }
+            @Override
+            public String toString() {
+                return String.format("{%d, %d, %d, %d}", x, y, w, h);
+            }
+        }
+
+        private int backgroundColor;
+        private List<Frame> frames;
+
+        private SheetImageLabel(BufferedImage bufferedImage) {
+            super(bufferedImage);
+        }
+
+        @Override
+        public void setImage(BufferedImage bim) {
+            super.setImage(bim);
+            this.backgroundColor = bim.getRGB(0, 0); // TODO: manual selection
+            this.frames = initFrames();
+            System.out.println(frames);
+        }
+
+        private List<Frame> initFrames() {
+            List<Frame> frames = new ArrayList<>();
+
+            for (int x = 0; x < bim.getWidth(); x++) {
+                for (int y = 0; y < bim.getHeight(); y++) {
+                    Frame f = lookForFrame(x, y);
+                    if (f != null) {
+                        frames.add(f);
+                    }
+                }
+            }
+
+            return frames;
+        }
+
+        private Frame lookForFrame(int x1, int y1) {
+            // Checks starting coord and north/west first.
+            if (isBG(x1, y1) || !isBG(x1 - 1, y1) || !isBG(x1, y1 - 1)) {
+                return null;
+            }
+
+            int w = 0;
+            int h = 0;
+            while (!isBG(x1 + w, y1)) w++;
+            while (!isBG(x1, y1 + h)) h++;
+
+            // checks for holes
+            for (int x = x1; x < x1 + w; x++) {
+                for (int y = y1; y < y1 + h; y++) {
+                    if (isBG(x, y)) return null;
+                }
+            }
+
+            // checks that there is a BG border
+            for (int x = x1; x < x1 + w; x++) {
+                if (!isBG(x, y1 - 1)) return null;
+                if (!isBG(x, y1 + h)) return null;
+            }
+            for (int y = y1; y < y1 + h; y++) {
+                if (!isBG(x1 - 1, y)) return null;
+                if (!isBG(x1 + w, y)) return null;
+            }
+
+            return new Frame(x1, y1, w, h);
+        }
+
+        private boolean isBG(int x, int y) {
+            if (x < 0 || y < 0) {
+                return true;
+            } else if (x > bim.getWidth() || y > bim.getHeight()) {
+                return false;
+            } else {
+                return bim.getRGB(x, y) == backgroundColor;
+            }
+        }
     }
 
     private JButton openFromImageButton;
@@ -109,7 +198,7 @@ public class SheetTool {
         if (returnVal == JFileChooser.APPROVE_OPTION) {
             File f = imageChooser.getSelectedFile();
             try {
-                ImageIO.write(toImageLabel.bufferedImage, "png", f);
+                ImageIO.write(toImageLabel.bim, "png", f);
                 infoLabel.setText("Successfully saved \"to\" image.");
             } catch (IOException e) {
                 infoLabel.setText("Could not write \"to\" image!");

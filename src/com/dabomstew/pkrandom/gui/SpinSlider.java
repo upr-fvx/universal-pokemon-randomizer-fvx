dcea17f4b38b58878bb3e292df68446e87a3acd6
Enrico <enbergmann@web.de>
2026-03-07 17:54:42 +0100

    Add SplinSlider class with ticks and labels

---
+++ src/com/dabomstew/pkrandom/gui/SpinSlider.java
@@ -0,0 +1,62 @@
+package com.dabomstew.pkrandom.gui;
+
+// Source - https://stackoverflow.com/a/6067986
+// Posted by trashgod, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-03-07, License - CC BY-SA 3.0
+
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class SpinSlider extends JPanel {
+
+    public static void main(String args[]) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame f = new JFrame("SpinSlider!");
+                f.add(new SpinSlider());
+                f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                f.pack();
+                f.setVisible(true);
+            }
+        });
+    }
+
+    public SpinSlider() {
+        this.setLayout(new FlowLayout());
+        final JSpinner spinner = new JSpinner();
+        final JSlider slider = new JSlider();
+        slider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                JSlider s = (JSlider) e.getSource();
+                spinner.setValue(s.getValue());
+            }
+        });
+        slider.setMinimum(-100);
+        slider.setMaximum(150);
+        slider.setValue(0);
+        slider.setMinorTickSpacing(5);
+        slider.setMajorTickSpacing(50);
+        slider.setPaintTicks(true);
+        slider.setPaintLabels(true);
+        this.add(slider);
+        spinner.setModel(new SpinnerNumberModel(0, -100, 150, 1));
+        spinner.setEditor(new JSpinner.NumberEditor(spinner, "0'%'"));
+        spinner.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                JSpinner s = (JSpinner) e.getSource();
+                slider.setValue((Integer) s.getValue());
+            }
+        });
+        this.add(spinner);
+    }
+}

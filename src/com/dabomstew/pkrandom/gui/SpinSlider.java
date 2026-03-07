91eb01ee61328795e098ba146e917254269d8c0e
Enrico <enbergmann@web.de>
2026-03-07 19:36:10 +0100

    Enable tooltip text and clean up example

--- src/com/dabomstew/pkrandom/gui/SpinSlider.java
+++ src/com/dabomstew/pkrandom/gui/SpinSlider.java
@@ -4,46 +4,41 @@ package com.dabomstew.pkrandom.gui;
 // Posted by trashgod, modified by community. See post 'Timeline' for change history
 // Retrieved 2026-03-07, License - CC BY-SA 3.0
 
-import java.awt.EventQueue;
 import java.awt.FlowLayout;
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 
 public class SpinSlider extends JPanel {
     private final JSpinner spinner = new JSpinner();
     private final JSlider slider = new JSlider();
 
     public SpinSlider() {
-        this.setLayout(new FlowLayout());
+        int MIN_VALUE = -100;
+        int MAX_VALUE = 150;
+        int DEFAULT_VALUE = 0;
 
-        slider.addChangeListener(new ChangeListener() {
-            @Override
-            public void stateChanged(ChangeEvent e) {
-                JSlider s = (JSlider) e.getSource();
-                spinner.setValue(s.getValue());
-            }
+        this.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+        slider.addChangeListener(e -> {
+            JSlider s = (JSlider) e.getSource();
+            spinner.setValue(s.getValue());
         });
-        slider.setMinimum(-100);
-        slider.setMaximum(150);
-        slider.setValue(0);
+        slider.setMinimum(MIN_VALUE);
+        slider.setMaximum(MAX_VALUE);
+        slider.setValue(DEFAULT_VALUE);
         slider.setMinorTickSpacing(5);
         slider.setMajorTickSpacing(50);
         slider.setPaintTicks(true);
         slider.setPaintLabels(true);
         this.add(slider);
-        spinner.setModel(new SpinnerNumberModel(0, -100, 150, 1));
+
+        spinner.setModel(new SpinnerNumberModel(DEFAULT_VALUE, MIN_VALUE, MAX_VALUE, 1));
         spinner.setEditor(new JSpinner.NumberEditor(spinner, "0'%'"));
-        spinner.addChangeListener(new ChangeListener() {
-            @Override
-            public void stateChanged(ChangeEvent e) {
-                JSpinner s = (JSpinner) e.getSource();
-                slider.setValue((Integer) s.getValue());
-            }
+        spinner.addChangeListener(e -> {
+            JSpinner s = (JSpinner) e.getSource();
+            slider.setValue((Integer) s.getValue());
         });
         this.add(spinner);
     }
@@ -56,8 +51,15 @@ public class SpinSlider extends JPanel {
         slider.setValue(value);
     }
 
+    @Override
     public void setEnabled(boolean enabled) {
         slider.setEnabled(enabled);
         spinner.setEnabled(enabled);
+    }
+
+    @Override
+    public void setToolTipText(String toolTipText) {
+        slider.setToolTipText(toolTipText);
+        spinner.setToolTipText(toolTipText);
     }
 }

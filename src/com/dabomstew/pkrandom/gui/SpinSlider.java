7ae7a639b86d1588f528c097c3f8dfa792e6dda8
Enrico <enbergmann@web.de>
2026-03-15 11:46:49 +0100

    Make slider resize on GUI resize

--- src/com/dabomstew/pkrandom/gui/SpinSlider.java
+++ src/com/dabomstew/pkrandom/gui/SpinSlider.java
@@ -4,43 +4,71 @@ package com.dabomstew.pkrandom.gui;
 // Posted by trashgod, modified by community. See post 'Timeline' for change history
 // Retrieved 2026-03-07, License - CC BY-SA 3.0
 
-import java.awt.FlowLayout;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
+import javax.swing.JFormattedTextField;
+import java.awt.*;
 
 public class SpinSlider extends JPanel {
     private final JSpinner spinner = new JSpinner();
     private final JSlider slider = new JSlider();
 
-    public SpinSlider() {
-        int MIN_VALUE = -100;
-        int MAX_VALUE = 155;
-        int DEFAULT_VALUE = 0;
+    public SpinSlider(int minValue, int maxValue, int defaultValue) {
+        this.setLayout(new java.awt.BorderLayout(5, 0));
 
-        this.setLayout(new FlowLayout(FlowLayout.LEFT));
-
+        // Construct slider
         slider.addChangeListener(e -> {
             JSlider s = (JSlider) e.getSource();
+            //slider.getValueIsAdjusting()
             spinner.setValue(s.getValue());
         });
-        slider.setMinimum(MIN_VALUE);
-        slider.setMaximum(MAX_VALUE);
-        slider.setValue(DEFAULT_VALUE);
+        slider.setMinimum(minValue);
+        slider.setMaximum(maxValue);
+        slider.setValue(defaultValue);
         slider.setMinorTickSpacing(5);
         slider.setMajorTickSpacing(50);
         slider.setPaintTicks(true);
         slider.setPaintLabels(true);
-        this.add(slider);
+        this.add(slider, java.awt.BorderLayout.CENTER);
 
-        spinner.setModel(new SpinnerNumberModel(DEFAULT_VALUE, MIN_VALUE, MAX_VALUE, 1));
-        spinner.setEditor(new JSpinner.NumberEditor(spinner, "0'%'"));
+        // Construct Spinner
+        spinner.setModel(new SpinnerNumberModel(defaultValue, minValue, maxValue, 1));
+
+        JSpinner.NumberEditor editor = new JSpinner.NumberEditor(spinner, "0'%'");
+        spinner.setEditor(editor);
+        autoSizeSpinner(spinner);
+
         spinner.addChangeListener(e -> {
             JSpinner s = (JSpinner) e.getSource();
             slider.setValue((Integer) s.getValue());
         });
-        this.add(spinner);
+        this.add(spinner, java.awt.BorderLayout.EAST);
+    }
+
+    public SpinSlider() {
+        this(-100, 155, 0);
+    }
+
+    private static void autoSizeSpinner(JSpinner spinner) {
+        JSpinner.NumberEditor editor = (JSpinner.NumberEditor) spinner.getEditor();
+        JFormattedTextField tf = editor.getTextField();
+
+        SpinnerNumberModel model = (SpinnerNumberModel) spinner.getModel();
+
+        String min = editor.getFormat().format(model.getMinimum());
+        String max = editor.getFormat().format(model.getMaximum());
+
+        String widest = min.length() > max.length() ? min : max;
+
+        FontMetrics fm = tf.getFontMetrics(tf.getFont());
+        int textWidth = fm.stringWidth(widest);
+
+        int width = textWidth + 40; // padding + arrow buttons
+        
+        Dimension d = spinner.getPreferredSize();
+        spinner.setPreferredSize(new Dimension(width, d.height));
     }
 
     public int getValue() {

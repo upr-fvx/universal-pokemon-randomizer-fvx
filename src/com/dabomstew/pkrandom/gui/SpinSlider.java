d5461d71d5af356bb7fc75df9e07b4e17c658cb0
Enrico <enbergmann@web.de>
2026-03-21 20:29:30 +0100

    Improve size of Spinner

--- src/com/dabomstew/pkrandom/gui/SpinSlider.java
+++ src/com/dabomstew/pkrandom/gui/SpinSlider.java
@@ -8,7 +8,6 @@ import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
-import javax.swing.JFormattedTextField;
 import java.awt.*;
 
 public class SpinSlider extends JPanel {
@@ -20,9 +19,9 @@ public class SpinSlider extends JPanel {
 
         // Construct slider
         slider.addChangeListener(e -> {
-            JSlider s = (JSlider) e.getSource();
-            //slider.getValueIsAdjusting()
-            spinner.setValue(s.getValue());
+            if (slider.getValueIsAdjusting()) {
+                spinner.setValue(slider.getValue());
+            }
         });
         slider.setMinimum(minValue);
         slider.setMaximum(maxValue);
@@ -31,44 +30,31 @@ public class SpinSlider extends JPanel {
         slider.setMajorTickSpacing(50);
         slider.setPaintTicks(true);
         slider.setPaintLabels(true);
-        this.add(slider, java.awt.BorderLayout.CENTER);
+        this.add(slider, BorderLayout.CENTER);
 
         // Construct Spinner
         spinner.setModel(new SpinnerNumberModel(defaultValue, minValue, maxValue, 1));
 
         JSpinner.NumberEditor editor = new JSpinner.NumberEditor(spinner, "0'%'");
         spinner.setEditor(editor);
-        autoSizeSpinner(spinner);
 
         spinner.addChangeListener(e -> {
-            JSpinner s = (JSpinner) e.getSource();
-            slider.setValue((Integer) s.getValue());
+            if (e.getSource() == spinner) {
+                slider.setValue((Integer) spinner.getValue());
+            }
         });
-        this.add(spinner, java.awt.BorderLayout.EAST);
+
+        JPanel spinnerPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));
+        spinnerPanel.add(spinner);
+
+        Dimension d = new Dimension(80, 30);
+        spinner.setPreferredSize(d);
+
+        this.add(spinnerPanel, BorderLayout.EAST);
     }
 
     public SpinSlider() {
         this(-100, 155, 0);
-    }
-
-    private static void autoSizeSpinner(JSpinner spinner) {
-        JSpinner.NumberEditor editor = (JSpinner.NumberEditor) spinner.getEditor();
-        JFormattedTextField tf = editor.getTextField();
-
-        SpinnerNumberModel model = (SpinnerNumberModel) spinner.getModel();
-
-        String min = editor.getFormat().format(model.getMinimum());
-        String max = editor.getFormat().format(model.getMaximum());
-
-        String widest = min.length() > max.length() ? min : max;
-
-        FontMetrics fm = tf.getFontMetrics(tf.getFont());
-        int textWidth = fm.stringWidth(widest);
-
-        int width = textWidth + 40; // padding + arrow buttons
-        
-        Dimension d = spinner.getPreferredSize();
-        spinner.setPreferredSize(new Dimension(width, d.height));
     }
 
     public int getValue() {

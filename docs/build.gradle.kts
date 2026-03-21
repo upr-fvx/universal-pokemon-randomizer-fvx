8625b2a969a08b02c2a6690e5f51b0c0c364e4f2
voliol <30631214+voliol@users.noreply.github.com>
2026-03-21 19:45:49 +0100

    implement finalizeReleaseNote except for downloads, rename upcoming version to v1.5.0
    (it only makes sense with how big it is)

--- docs/build.gradle.kts
+++ docs/build.gradle.kts
@@ -1,16 +1,25 @@
 import java.nio.charset.StandardCharsets
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 dependencies {
     implementation(project(":random")) // to get Version
 }
 
-val RELEASE_NOTES_PATH = "docs/src/_release_notes"
+val releaseNotesPath = "docs/src/_release_notes"
+val templatePath = "$releaseNotesPath/release-note-template.md"
+val nextPath = "$releaseNotesPath/release-note-next.md"
+
+// identifies stray [] but not when used in Markdown links
+val squareBracketRegex = "(?<!\\])\\[[^\\]]*\\](?![\\[\\(])".toRegex()
+// identifies html-style comments that end with newline
+val commentRegex = "<!--[\\s\\S]*?-->\\r?\\n".toRegex()
 
 tasks.register("checkVersionUpdated") {
     dependsOn(":random:getVersionName")
     doLast {
         val releaseName = (rootProject.extra["randomizerVersion"] as String).replace(".", "_")
-        val posReleaseNotePath = "$RELEASE_NOTES_PATH/$releaseName.md"
+        val posReleaseNotePath = "$releaseNotesPath/$releaseName.md"
         println(File(posReleaseNotePath).absolutePath)
         if (File(posReleaseNotePath).exists()) {
             error("Could not finalize release note; file already exists with the file name the finalized release note " +
@@ -20,15 +29,11 @@ tasks.register("checkVersionUpdated") {
     }
 }
 
-// identifies stray [] but not when used in markdown links
-val SQUARE_BRACKET_REGEX = "(?<!\\])\\[[^\\]]*\\](?![\\[\\(])".toRegex()
-
 tasks.register("checkReleaseNoteDone") {
     doLast {
-        val rnPath = "$RELEASE_NOTES_PATH/release-note-next.md"
-        val text = File(rnPath).readText(StandardCharsets.UTF_8)
-        if (SQUARE_BRACKET_REGEX.find(text) != null) {
-            val matches = SQUARE_BRACKET_REGEX.findAll(text)
+        val text = File(nextPath).readText(StandardCharsets.UTF_8)
+        if (squareBracketRegex.find(text) != null) {
+            val matches = squareBracketRegex.findAll(text)
             var matchesString = ""
             matches.iterator().forEach {
                 matchResult -> matchesString += "\t" + matchResult.value + "\n"
@@ -42,4 +47,24 @@ tasks.register("checkReleaseNoteDone") {
 tasks.register("checkFinalizeReleaseNote") {
     dependsOn("checkVersionUpdated")
     dependsOn("checkReleaseNoteDone")
+}
+
+tasks.register("finalizeReleaseNote") {
+    dependsOn(":random:getVersionName")
+    dependsOn("checkFinalizeReleaseNote")
+    doLast {
+        val releaseName = (rootProject.extra["randomizerVersion"] as String).replace(".", "_")
+        val releaseNamePath = "$releaseNotesPath/$releaseName.md"
+
+        var text = File(nextPath).readText(StandardCharsets.UTF_8)
+        val sb = StringBuilder()
+        sb.append("---\n")
+        sb.append("name: ${rootProject.extra["randomizerVersion"] as String}\n")
+        sb.append("date: ${LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))}\n")
+        sb.append("---\n")
+        sb.append(text.replace(commentRegex, ""))
+
+        File(releaseNamePath).writeText(sb.toString(), StandardCharsets.UTF_8)
+        File(templatePath).copyTo(File(nextPath), true)
+    }
 }

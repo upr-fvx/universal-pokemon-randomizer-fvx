a17a5b3e191db65657daa1878f9cfdecef859729
voliol <30631214+voliol@users.noreply.github.com>
2026-04-21 19:02:24 +0200

    (Gen5) fix corphish loading failure

--- romio/src/main/java/com/uprfvx/romio/romhandlers/Gen5RomHandler.java
+++ romio/src/main/java/com/uprfvx/romio/romhandlers/Gen5RomHandler.java
@@ -847,7 +847,11 @@ public class Gen5RomHandler extends AbstractDSRomHandler {
             int speciesID = readWord(data, offset + i * 4) & 0x7FF;
             int forme = readWord(data, offset + i * 4) >> 11;
             Species pk = pokes[speciesID];
-            if (forme != 30 && forme != 31) {
+            // In Black 2, there is one (1) faulty encounter for Corphish "forme 1",
+            // (probably because of Basculin data right next to it, it also has forme 1 only in Black 2).
+            // In any case we don't want that GameFreak typo to crash the Randomizer,
+            // so we ignore forme checking for Corphish.
+            if (forme != 30 && forme != 31 && speciesID != SpeciesIDs.corphish) {
                 pk = pk.getForme(forme);
             }
             enc.setSpecies(pk);

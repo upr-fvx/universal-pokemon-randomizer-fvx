1e2649d21a2c83766903d63ec92e8dd45d8aba76
Stella Hack <foxoftheasterisk@gmail.com>
2026-08-02 13:03:26 -0700

    Added a number of SettingsManager tests.
    ...they do not have the desired results.

--- random/src/test/java/com/uprfvx/random/settings/SettingsManagerTest.java
+++ random/src/test/java/com/uprfvx/random/settings/SettingsManagerTest.java
@@ -1,21 +1,75 @@
 package com.uprfvx.random.settings;
 
+import com.uprfvx.random.settings.definitions.SettingDefinition;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SettingsManagerTest {
 
     @Test
-    public void canConstruct()
-    {
+    public void canConstruct() {
         SettingsManager manager = new SettingsManager();
     }
 
     @Test
-    public void canGetValue()
-    {
+    public void canGetBooleanValue() {
         SettingsManager manager = new SettingsManager();
 
         boolean value = manager.getSetting("LimitPokemon");
         assert(value == false);
+    }
+
+    @Test
+    public void canSetBooleanValue() {
+        SettingsManager manager = new SettingsManager();
+
+        manager.setSetting("LimitPokemon", true);
+        boolean value = manager.getSetting("LimitPokemon");
+        assert(value == true);
+    }
+
+    @Test
+    public void canGetIntValue() {
+        SettingsManager manager = new SettingsManager();
+
+        int value = manager.getSetting("UpdateMovesToGeneration");
+        assert (value == 9);
+    }
+
+    @Test
+    public void canSetIntValue() {
+        SettingsManager manager = new SettingsManager();
+
+        manager.setSetting("UpdateMovesToGeneration", 7);
+        int value = manager.getSetting("UpdateMovesToGeneration");
+        assert (value == 7);
+    }
+
+    @Test
+    public void getWrongTypeThrows() {
+        SettingsManager manager = new SettingsManager();
+
+        Exception e = assertThrows(ClassCastException.class, () -> {
+            int value = manager.getSetting("LimitPokemon");
+        });
+    }
+
+    @Test
+    public void setWrongTypeThrows() {
+        SettingsManager manager = new SettingsManager();
+
+        Exception e = assertThrows(ClassCastException.class, () -> {
+            manager.setSetting("LimitPokemon", 3.0);
+        });
+    }
+
+    @Test
+    public void getWrongTypeDefinitionThrows() {
+        SettingsManager manager = new SettingsManager();
+
+        Exception e = assertThrows(ClassCastException.class, () -> {
+            SettingDefinition<Integer> definition = manager.getSettingDefinition("LimitPokemon");
+        });
     }
 }
